74162c067dad789fe0d37c47615e6618265dd4f8
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-03-01 13:53:57 +0000

    initializing spring boot project

---
+++ src/main/java/com/fsdm/itclub/certify/CertifyItclubApplication.java
@@ -0,0 +1,13 @@
+package com.fsdm.itclub.certify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifyItclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertifyItclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsdm/itclub/certify/CertifyItclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsdm.itclub.certify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertifyItclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
